8a77c68d73eb94d1fd7eb763a001e4e07c06e328
KateScher <102732149+KateScher@users.noreply.github.com>
2022-10-22 16:23:30 +0500

    Add files via upload

--- js/login-popup.js
+++ js/login-popup.js
@@ -1,5 +1,6 @@
 let loginButton = document.querySelector('.login-button');
 let loginPopup = document.querySelector('.login-popup');
+let popupBackdrop = document.querySelector('.popup-backdrop');
 
 loginButton.addEventListener('click', showPopup);
 
@@ -7,5 +8,6 @@ function showPopup(evt) {
 console.log(evt);
 evt.preventDefault();
 loginPopup.classList.toggle('show-popup');
+popupBackdrop.classList('show-backdrop');
 }
 
